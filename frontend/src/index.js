ba33b27e788994d675616c024e29e00fe74fdebc
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-02-23 10:47:44 -0600

    adds Single and Post List components and react-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles/index.css";
+import registerServiceWorker from "./registerServiceWorker";
+// import components
+import Main from "./component/Main";
+import Single from "./component/Single";
+import PostList from "./component/PostList";
+// import react-router deps
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PostList} />
+      <Route path="/view/:postId" component={Single} />
+    </Route>
+  </Router>
+);
+
+ReactDOM.render(router, document.getElementById("root"));
 registerServiceWorker();
