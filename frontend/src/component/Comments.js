b8533a2cd89af27bcb64d2f021c2edbbc76270b2
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2018-03-14 18:00:47 -0500

    routes to single post and starts comments comp

---
+++ frontend/src/component/Comments.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+//import comments
+class Comments extends Component {
+  render() {
+    return (
+      <div className="comment">
+        <p>I am a comment</p>
+      </div>
+    );
+  }
+}
+
+export default Comments;
--- frontend/src/component/Single.js
+++ frontend/src/component/Single.js
@@ -1,8 +1,21 @@
 import React, { Component } from "react";
-
+import Post from "./Post";
+import Comments from "./Comments";
 class Single extends Component {
   render() {
-    return <div className="single-post">I am a Single Post</div>;
+    // index of post
+    const i = this.props.posts.findIndex(
+      post => post.id === this.props.params.postId
+    );
+    // get us the post
+    const post = this.props.posts[i];
+
+    return (
+      <div className="single-post">
+        <Post i={i} post={post} {...this.props} />
+        <Comments />
+      </div>
+    );
   }
 }
 
